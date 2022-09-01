a253914f609b3286c2f089f0324493be81f18811
SourCreamNIndi <niccam53@kingshigh.school.nz>
2022-09-01 14:58:25 +1200

    rt

---
+++ DoorAndButton.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _ready():
+	$AnimationPlayer.play("ButtonUp")
+	$AnimationPlayer.play("DoorClosed") 
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		$AnimationPlayer.play("ButtonDown")
+		$AnimationPlayer.play("DoorOpen")
+		
