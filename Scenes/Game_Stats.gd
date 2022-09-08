276c9df0a75d436fa54e0b3e5871430312fb593b
SourCreamNIndi <niccam53@kingshigh.school.nz>
2022-09-08 12:55:56 +1200

    Rollback to uncorrupted edit

---
+++ Scenes/Game_Stats.gd
@@ -0,0 +1,32 @@
+extends Node
+
+onready var game_start_time = OS.get_ticks_msec()
+var current_spawn = null
+
+func reset():
+	current_spawn = null
+	get_tree().reload_current_scene()
+	game_start_time = OS.get_ticks_msec()
+	
+	
+	
+func check_reset():
+	if current_spawn == null:
+		reset()
+	else:
+		return false
+		
+func set_spawn(spawn):
+	current_spawn = spawn
+	
+func get_spawn():
+	return current_spawn
+	
+	"""
+func get_time():
+	var current_time = OS.get_ticks_msec() - game_start_time
+	var minutes = current_time/1000/60
+	var seconds = current_time/1000%60
+	var ms = current_time%1000/10
+	print(str(minutes),":",str(seconds),":",str(ms))
+	"""
--- swinnging trap.gd
+++ swinnging trap.gd
@@ -8,4 +8,5 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
 		print("Player dead")
-		
+		if GameStats.check_reset() == false:
+			body.global_position = GameStats.get_spawn().global_position
